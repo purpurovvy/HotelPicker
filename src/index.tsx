022c8cee0358ae8aaa3cf232f255799c0c09933a
Purpurovvy <purpurovvy@gmail.com>
2023-04-01 11:05:46 +0200

    App init

---
+++ src/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {BrowserRouter} from "react-router-dom";
+import {theme} from "./theme";
+import {ThemeProvider} from "@mui/material";
+
+const queryClient = new QueryClient()
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+
+root.render(
+    <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </ThemeProvider>
+    </QueryClientProvider>
+);
