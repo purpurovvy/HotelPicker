93ab6600d628999623b473708ffcc114350da099
Purpurovvy <purpurovvy@gmail.com>
2023-04-16 14:31:59 +0200

    Modified Counter tests

--- src/components/commons/Counter/Counter.test.tsx
+++ src/components/commons/Counter/Counter.test.tsx
@@ -3,26 +3,29 @@ import { render, fireEvent } from "@testing-library/react";
 import { Counter } from "./Counter";
 
 describe("Counter", () => {
+  const ParentMockWithCounter = () => {
+    const [value, setValue] = React.useState(0);
+    return <Counter title="Test" value={value} changeValue={setValue} />;
+  };
   it("Should render provided title and value correctly", () => {
-    const { getByTestId } = render(
-      <Counter title="Test" value={3} changeValue={() => {}} />
-    );
+    const { getByTestId } = render(<ParentMockWithCounter />);
     const counter = getByTestId("Test-counter");
     expect(counter).toHaveTextContent("Test:");
-    expect(counter).toHaveTextContent("3");
+    expect(counter).toHaveTextContent("0");
   });
 
-  it("increments the value when the increment button is clicked", () => {
-    const changeValue = jest.fn();
-    const { getByTestId } = render(
-      <Counter title="Test" value={3} changeValue={changeValue} />
-    );
+  it("Should change the value given by parent on buttons click", () => {
+    const { getByTestId } = render(<ParentMockWithCounter />);
     const incrementButton = getByTestId("counter-Test-incrementButton");
-    fireEvent.click(incrementButton);
-    expect(changeValue).toHaveBeenCalledTimes(1);
-
     const decrementButton = getByTestId("counter-Test-decrementButton");
+    const counter = getByTestId("Test-counter");
+
+    fireEvent.click(incrementButton);
+    expect(counter).toHaveTextContent("1");
+    expect(counter).not.toHaveTextContent("0");
+
     fireEvent.click(decrementButton);
-    expect(changeValue).toHaveBeenCalledTimes(2);
+    expect(counter).toHaveTextContent("0");
+    expect(counter).not.toHaveTextContent("1");
   });
 });
